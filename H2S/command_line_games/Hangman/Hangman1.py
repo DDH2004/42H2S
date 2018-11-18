432c1b8ecd5dbdf1fb5b9301ca9af7a77205d52a
Akhil Devarasetty <adevaras@e1z2r3p43.42.us.org>
2018-11-17 16:10:43 -0800

    fwgwrsg

--- H2S/command_line_games/Hangman/Hangman1.py
+++ H2S/command_line_games/Hangman/Hangman1.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import random
+
 import sys
-lives = 6
 i = 0
 underscore = []
 selection = raw_input("Select easy, medium, or hard: ")
 
 
-
 if selection == "easy":
+	lives = 6
 	easy = random.choice(open("easy_mode.txt").readline().split())
 	i = 0
 	while i < len(easy):
@@ -25,20 +25,27 @@ if selection == "easy":
 				print x,
 			lives = lives - 1
 		elif guess in easy:
-			pos = easy.index(guess)
-			underscore[pos] = guess
+			correctLetters = 0
+			for i in range(len(easy)):
+				if easy[i] == guess:
+					underscore[i] = guess
+			for i in range(len(easy)):
+				if easy[i] == underscore[i]:
+					correctLetters += 1
+			if (correctLetters == len(easy)):
+				print("You win")
+				break
 			for x in underscore:
 				print x,
 		else:
 			print("That is not the correct letter.")
 			print underscore
 			lives = lives - 1
-		print
 		i = i + 1
 		print ("remaining lives " + str(lives))
 	if 0 == lives:
 		print("You ran out of lives")
-		print("The word is: " + easy)
+		print("The word was: " + easy)
 
 
 
@@ -53,25 +60,43 @@ elif selection == "medium":
 
 
 elif selection == "hard":
- 	hard = random.choice(open("hard_mode.txt").readline().split())
- 	print(hard)
- 	i = 0
- 	while i < len(hard):
- 		underscore.append("_")
+	lives = 10
+	hard = random.choice(open("hard_mode.txt").readline().split())
+	i = 0
+	while i < len(hard):
+		underscore.append("_")
 		i = i + 1
 	for x in underscore:
 		print x,
 	while 0 < lives:
 		print hard
 		guess = raw_input("LETTER: ")
-		if guess in hard:
-			pos = hard.index(guess)
-			underscore[pos] = guess
+		if len(guess) > 1:
+			print("1 letter only.")
+			for x in underscore:
+				print x,
+			lives = lives - 1
+		elif guess in hard:
+			correctLetters = 0
+			for i in range(len(hard)):
+				if hard[i] == guess:
+					underscore[i] = guess
+			for i in range(len(hard)):
+				if hard[i] == underscore[i]:
+					correctLetters += 1
+			if (correctLetters == len(hard)):
+				print(hard)
+				print("You win")
+				break
 			for x in underscore:
 				print x,
 		else:
 			print("That is not the correct letter.")
-			print underscore
+			for x in underscore:
+				print x
 			lives = lives - 1
 		i = i + 1
 		print ("remaining lives " + str(lives))
+	if 0 == lives:
+		print("You ran out of lives")
+		print("The word was: " + hard)
